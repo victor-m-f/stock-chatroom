d6cbd946d7b54b8e9b185669a93d1602c4b1c031
Victor Fernandes <victor.m.fernandes@outlook.com>
2022-07-09 22:58:56 -0300

    removing old worker project

--- src/StockChatroom.Worker1/Configuration/AutoMapper/AutoMapperConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StockChatroom.Worker.Configuration.AutoMapper;
-
-public static class AutoMapperConfiguration
-{
-    public static void ConfigureAutoMapper(this IServiceCollection services) =>
-        _ = services.AddAutoMapper(typeof(AutoMapperProfile));
-}
